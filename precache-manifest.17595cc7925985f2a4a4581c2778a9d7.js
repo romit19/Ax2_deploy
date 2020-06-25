42e78b2a735b9ce3528beaf57ca071b031441ab8
romit19 <romitsolanki2012@gmail.com>
2020-06-25 15:21:00 +0530

    Updates

---
+++ precache-manifest.17595cc7925985f2a4a4581c2778a9d7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "189d531ef7891b65f297a0449e0a5d21",
+    "url": "/Ax2_deploy/index.html"
+  },
+  {
+    "revision": "7f6b4df10194c0db09e0",
+    "url": "/Ax2_deploy/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "290da1bd9d4cff9cdffc",
+    "url": "/Ax2_deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7f6b4df10194c0db09e0",
+    "url": "/Ax2_deploy/static/js/2.e488afae.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Ax2_deploy/static/js/2.e488afae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "290da1bd9d4cff9cdffc",
+    "url": "/Ax2_deploy/static/js/main.59423d5f.chunk.js"
+  },
+  {
+    "revision": "bfb7169d931606f10dde",
+    "url": "/Ax2_deploy/static/js/runtime-main.54c6376f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Ax2_deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
